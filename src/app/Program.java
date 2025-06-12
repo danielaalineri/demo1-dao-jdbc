6132a2c52dadf29fcef461ac0c9bda48e5569409
danielaalineri <danielalineri@gmail.com>
2025-06-11 21:59:18 -0300

    SellerDaoJDBC and DaoFactory - Fixes

--- src/app/Program.java
+++ src/app/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
-		SellerDao sellerDao = DaoFactory.creaSellerDao();
+		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
 		System.out.println(seller);
 	}
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
-	public static SellerDao creaSellerDao() {
-		return new SellerDaoJDBC();
+	public static SellerDao creatSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
